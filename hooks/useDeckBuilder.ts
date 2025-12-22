f2b10ea3e54d3baf8267dd2d6c28917f0ea971fe
Drakontia <mhden@drakontia.com>
2025-12-23 05:20:23 +0900

    キャラクターを選択し直すと、曖昧な記憶のポイントがリセットされるようにする

--- hooks/useDeckBuilder.ts
+++ hooks/useDeckBuilder.ts
@@ -48,6 +48,7 @@ export function useDeckBuilder(initialDeck?: Deck) {
         character,
         cards: deckCards,
         removedCards: new Map(),
+        copiedCards: new Map(),
         convertedCards: new Map()
       };
     });
